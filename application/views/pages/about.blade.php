e0bcba443f331e48788da8387df68f881ff98b58
Ye Myat Min <yemyat@gmail.com>
2013-08-25 14:06:40 +0800

    Remove dummy data

--- application/views/pages/about.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>pages.about</h1>
-
-<p>This view has been auto-generated to accompany the Pages_Controller's action_about()</p>
--- application/views/pages/fangate.blade.php
+++ application/views/pages/fangate.blade.php
@@ -1,20 +1,4 @@
 @layout('layouts/app')
 @section('content')
-  <div class="jumbotron">
-    <img src="{{URL::to_asset('img/manhunt.jpg')}}" alt="">
-
-    <p>PREDICT which Manhunt Contestant will be “Mr ORIS” and stand a chance to win ORIS premium timepieces worth more than $2,200!</p>
-
-    <div class="row">
-      <img src="{{URL::to_asset('img/prize1.jpg')}}" alt="">
-    </div>
-
-    <div class="row">
-      <p class="small">Contest ends 31st August 2013, 1430hrs. One lucky winner with the correct “Mr ORIS” prediction will be picked to win one of the ORIS premium timepieces.</p>
-    </div>
-  </div>
-
-  <div class="arrow">
-    <img src="{{URL::to_asset('img/arrow.png')}}" alt="">
-  </div>
+Fangate
 @endsection
